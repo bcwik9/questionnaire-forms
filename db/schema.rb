8e928b2904eedbb15e1e20874fe9982a7db91ff4
Ubuntu <ubuntu@ip-172-31-41-115.ec2.internal>
2015-02-05 00:20:38 +0000

    add new question scaffold which is essentially a question and an answer

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205000935) do
+ActiveRecord::Schema.define(version: 20150205002007) do
 
   create_table "questionnaires", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+  end
+
+  create_table "questions", force: true do |t|
+    t.string   "content"
+    t.string   "answer"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
