606033943685ce657c52c660adf52fc07b562b90
Ubuntu <ubuntu@ip-172-31-41-115.ec2.internal>
2015-02-05 00:16:41 +0000

    added a name to a questionnaire

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204195517) do
+ActiveRecord::Schema.define(version: 20150205000935) do
 
   create_table "questionnaires", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
 end
