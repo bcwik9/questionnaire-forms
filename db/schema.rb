7fe12b8d550a07bedc4e223613a3d9f5cc8aa8be
Ubuntu <ubuntu@ip-172-31-41-115.ec2.internal>
2015-02-07 22:56:33 +0000

    format some .erb files and add Answer model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205214245) do
+ActiveRecord::Schema.define(version: 20150207225252) do
+
+  create_table "answers", force: true do |t|
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "questionnaires", force: true do |t|
     t.datetime "created_at"
