7c8e77ad1dca6734f23e93925f999255ec5338d9
Ubuntu <ubuntu@ip-172-31-41-115.ec2.internal>
2015-02-11 01:28:08 +0000

    two new tests nearly working

--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe Response do
+
+  let(:response) {
+    FactoryGirl.create :response
+  }
+
+  let(:valid_attributes) {
+    response_attrs = FactoryGirl.attributes_for :response
+    return response_attrs
+  }
+
   it "should have at least one answer" do
-    pending "add functionality!"
+    expect(response.answers.size).to be > 0
   end
 
   it "should have a user who submitted it" do
-    pending "add functionality!"
+    expect(response.user).to eq(valid_attributes[:user]) 
   end
 end
